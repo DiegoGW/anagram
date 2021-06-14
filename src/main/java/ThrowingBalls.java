6ba90d350e8a0e75f5f085b8918cd8727f18f5f9
Diego Gawenda <dgawenda@theappraisallane.com>
2021-06-14 13:44:49 -0300

    Fixing description

--- src/main/java/ThrowingBalls.java
+++ src/main/java/ThrowingBalls.java
@@ -1,32 +1,51 @@
 import java.util.Random;
 
+/**
+ * Throwing balls from a N-store Building
+ *
+ * Considering that the building has N floors and that the balls
+ * that are thrown from a higher floor than F get broken, then a possible solution
+ * to find F could be a liner search.
+ * Going down from N, N-1, N-2.. until the ball doesn't get broken.
+ * This will return a value for F, but in a linear search, and this doesn't meet
+ * the lgN throws requirement.
+ * So a possible option could be to find F floor with a BinarySearch (O lgN), that will speed
+ * up the process.
+ * For example:
+ * 1- If throwing a ball from floor N gets broken, then try with N/2 until you find a floor where the ball
+ * doesn't get broken. Once you find a floor where the ball doesn't get broken, then you
+ * redefine the min and max range of floors to find the lower floor where the ball starts getting broken.
+ *
+ * 2- To find F-floor with O(2 lg F), a possible solution could be to start from floor 1, next 2, 4, 8 (2^i)
+ * Once the ball gets broken (after lg F steps), a binary search could be done in the reduced range.
+ * As the range < F, then the number of searches < log F.
+ */
+
+
+
 public class ThrowingBalls {
 
-    private static int floorBroken = 0;
+    private static int floorBroken;
 
     /**
      * n = number of floors of the building
-     * f = number of the floor that defines if a ball will get broken or not
-     * bb = function that returns if a throwing a ball from a specific floor will get broken
-     * To find "f" in ~lg n throws it could be applied a binarySearch considering
-     * the height of the building = n floors and the function that returns true
-     * if a ball gets broken from a sepecific floor
-     *
-     * @param args
+     * floorBroken = number of the floor that defines if a ball will get broken or not
      */
-
     public static void main(String args[]){
         int n = 1000;
         Random rand = new Random();
         floorBroken = rand.nextInt(n);
 
         System.out.println("The random floor F is: " + floorBroken);
-        System.out.println("The found floor F is: " + findFloorF(n,0));
-        System.out.println("Other way to find floor F is: " + findFloorFOtherWay());
+        System.out.println("Floor F found in lgN: " + findFloorF(n,0));
+        System.out.println("Floor F found in 2lgF: " + findFloorFOtherWay());
 
 
     }
 
+    /**
+     * Function that returns if throwing a ball from a specific floor will get broken
+     */
     public static boolean isBallBroken(int floor){
         return (floor >= floorBroken);
     }
@@ -34,22 +53,16 @@ public class ThrowingBalls {
     /**
      * Find floor F based on the N floors that has a building
      * Big O = lg N
-     * Precondition: N >= F
-     * @param maxFloor
-     * @param minFloor
-     * @return
+     * Precondition: N >= F >= 1
      */
     public static int findFloorF(int maxFloor, int minFloor){
-        //Base condition check
+        //Precondition check
         if (maxFloor < minFloor){
             return -1;
         }
+        //Precondition check
         if (maxFloor==0){
             return 0;
-        }else if ((maxFloor==1) && (isBallBroken(maxFloor))){
-            return 0;
-        }else if ((maxFloor==1) && (!isBallBroken(maxFloor))){
-            return 1;
         }else if ((maxFloor == minFloor+1) && (!isBallBroken(maxFloor))){
             return minFloor;
         }else if ((maxFloor == minFloor+1) && (isBallBroken(maxFloor))) {
@@ -71,10 +84,23 @@ public class ThrowingBalls {
      */
     public static int findFloorFOtherWay(){
         int i = 0;
-        while (!isBallBroken(2^i)){
+        while (!isBallBroken(intPower(2,i))){
             i++;
         }
-        return findFloorF(2^i, 2^(i-1));
+        if (i==0){
+            return findFloorF(intPower(2,i), 0);
+        }else{
+            return findFloorF(intPower(2,i), intPower(2,i-1));
+        }
+
+    }
+
+    private static int intPower(int x, int y){
+        int result=1;
+        for (int i=0; i<y; i++){
+            result *=x;
+        }
+        return result;
     }
 
 
